8eb4acf7178aed33daabd395c4b0c7e5b1e82a92
Damian Iwanicki <damian.iwanicki@rwth-aachen.de>
2017-12-06 14:29:22 +0100

    changed detector setup to fiber + logical detection volume at fiberend

--- src/B1DetectorConstruction.cc
+++ src/B1DetectorConstruction.cc
@@ -33,6 +33,7 @@
 #include "G4RunManager.hh"
 #include "G4NistManager.hh"
 #include "G4Box.hh"
+#include "G4Tubs.hh"
 #include "G4Cons.hh"
 #include "G4Orb.hh"
 #include "G4Sphere.hh"
@@ -40,6 +41,9 @@
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4Color.hh"
+#include "G4VisAttributes.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -63,7 +67,6 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   // Envelope parameters
   //
   G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
-  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
    
   // Option to switch on/off checking of volumes overlaps
   //
@@ -94,83 +97,57 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                       false,                 //no boolean operation
                       0,                     //copy number
                       checkOverlaps);        //overlaps checking
+
+  G4VisAttributes * visAttWorld = new G4VisAttributes(G4Colour(0.,1.,1.));
+  visAttWorld->SetForceWireframe(true);
+  logicWorld->SetVisAttributes(visAttWorld);
                      
-  //     
-  // Envelope
-  //  
-  G4Box* solidEnv =    
-    new G4Box("Envelope",                    //its name
-        0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
-      
-  G4LogicalVolume* logicEnv =                         
-    new G4LogicalVolume(solidEnv,            //its solid
-                        env_mat,             //its material
-                        "Envelope");         //its name
-               
-  new G4PVPlacement(0,                       //no rotation
-                    G4ThreeVector(),         //at (0,0,0)
-                    logicEnv,                //its logical volume
-                    "Envelope",              //its name
-                    logicWorld,              //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
- 
   //     
   // Shape 1
   //  
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
-        
-  // Conical section shape       
-  G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
-  G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
-  G4double shape1_hz = 3.*cm;
-  G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
-  G4Cons* solidShape1 =    
-    new G4Cons("Shape1", 
-    shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
-    shape1_phimin, shape1_phimax);
+  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_PLEXIGLASS");
+  G4ThreeVector pos1 = G4ThreeVector(0, 0*cm, 0*cm);
+  G4double fiberlength = 10.*cm;
+              
+  G4Tubs* solidShape1 = new G4Tubs("solidShape1", 0, 1.*cm, fiberlength/2., 0, CLHEP::twopi*radian);
                       
   G4LogicalVolume* logicShape1 =                         
     new G4LogicalVolume(solidShape1,         //its solid
                         shape1_mat,          //its material
-                        "Shape1");           //its name
+                        "logicShape1");           //its name
                
   new G4PVPlacement(0,                       //no rotation
                     pos1,                    //at position
                     logicShape1,             //its logical volume
                     "Shape1",                //its name
-                    logicEnv,                //its mother  volume
+                    logicWorld,                //its mother  volume
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
 
+  G4VisAttributes * visAtt = new G4VisAttributes(G4Colour(0.,1.,1.));
+  visAtt->SetForceWireframe(true);
+  logicShape1->SetVisAttributes(visAtt);
+
   //     
   // Shape 2
   //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1*cm, 7*cm);
+  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_AIR");
+  G4double detectorLength = 1e-3*cm;
+  G4ThreeVector pos2 = G4ThreeVector(0, 0, fiberlength/2. + detectorLength/2. + 1e-9*cm);
 
-  // Trapezoid shape       
-  G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
-  G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
-  G4double shape2_dz  = 6*cm;      
-  G4Trd* solidShape2 =    
-    new G4Trd("Shape2",                      //its name
-              0.5*shape2_dxa, 0.5*shape2_dxb, 
-              0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz); //its size
-                
-  G4LogicalVolume* logicShape2 =                         
-    new G4LogicalVolume(solidShape2,         //its solid
+  G4Tubs* solidShape2 = new G4Tubs("solidShape2", 0, 1.*cm, detectorLength/2., 0, CLHEP::twopi*radian);
+
+  G4LogicalVolume* logicShape2 =
+  new G4LogicalVolume(solidShape2,         //its solid
                         shape2_mat,          //its material
-                        "Shape2");           //its name
+                        "logicShape2");           //its name
                
   new G4PVPlacement(0,                       //no rotation
                     pos2,                    //at position
                     logicShape2,             //its logical volume
                     "Shape2",                //its name
-                    logicEnv,                //its mother  volume
+                    logicWorld,                //its mother  volume
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
